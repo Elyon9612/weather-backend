66f1cf0d6a45bfeb98405c8c9dc2b07f8fb550a9
Elyon9612 <lihongh@oregonstate.edu>
2022-02-04 19:46:54 -0800

    update app.js

--- app.js
+++ app.js
@@ -6,15 +6,99 @@ var app = express()
 app.listen(3000)
 console.log('Node.js Express server id running on port 3000...')
 
-app.get('/data/2.5/weather', get_weather)
-app.get('/v1/hello', get_hello)
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io'];
+	const origin = req.headers.origin;
+		
+	if (allowedOrigins.includes(origin)) {
+	  res.setHeader('Access-Control-Allow-Origin', origin);
+	}
 
+	// Request methods you wish to allow eg: GET, POST, OPTIONS, PUT, PATCH, DELETE
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	
+	const bearerHeader = req.headers["authorization"];
+	if (typeof bearerHeader != "undefined") {
+	  const bearer = bearerHeader.split(" ");
+	  const bearerToken = bearer[1];
+	  req.token = bearerToken;
+	  next();
+	}
+
+	// Pass to next layer of middleware
+	next();
+  });
+
+
+app.get('/data/2.5/weather', get_weather)
+app.get("/v1/weather", get_weather)
+app.post("/v1/auth", auth);
+app.get("/v1/hello", greet);
 
 function get_weather(request, response) {
-    response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":281.56,"feels_like":281.56,"temp_min":279.46,"temp_max":283.9,"pressure":1032,"humidity":73},"visibility":10000,"wind":{"speed":0.89,"deg":113,"gust":1.34},"clouds":{"all":75},"dt":1642803894,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642779731,"sunset":1642813576},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+    response.json( {
+      "coord": {
+        "lon": -123.262,
+        "lat": 44.5646
+      },
+      "weather": [
+        {
+          "id": 741,
+          "main": "Fog",
+          "description": "fog",
+          "icon": "50n"
+        }
+      ],
+      "base": "stations",
+      "main": {
+        "temp": 276.57,
+        "feels_like": 276.57,
+        "temp_min": 274.72,
+        "temp_max": 279.05,
+        "pressure": 1024,
+        "humidity": 79
+      },
+      "visibility": 805,
+      "wind": {
+        "speed": 0,
+        "deg": 0
+      },
+      "clouds": {
+        "all": 100
+      },
+      "dt": 1642320503,
+      "sys": {
+        "type": 2,
+        "id": 2012991,
+        "country": "US",
+        "sunrise": 1642347934,
+        "sunset": 1642381185
+      },
+      "timezone": -28800,
+      "id": 5720727,
+      "name": "Corvallis",
+      "cod": 200
+    } )
 }
 
-function get_hello(request, response){
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.json("FK bitch")
+function greet(req, res) {
+    if (req.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkthcmwgQWRyaWFubyIsImlhdCI6MTUxNjIzOTAyMn0.STyNSCjMt9cKNL6YVfLRTBabPbzbW1FDRebDqTwC2-c") {
+      res.json({
+        "greeting": "Elyon says Hello" 
+      });
+    }
+    else {
+      res.sendStatus(401);
+    }
+}
+
+function auth(req, res) {
+    res.json( {
+      "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkthcmwgQWRyaWFubyIsImlhdCI6MTUxNjIzOTAyMn0.STyNSCjMt9cKNL6YVfLRTBabPbzbW1FDRebDqTwC2-c",
+      "expires": "2022-02-11T23:59:59.999-08:00"
+    } )
 }
